af46395f7f9e2c7de3d22476c8bc7f6b678b7c02
Huan Tran <103070104+huantran123@users.noreply.github.com>
2023-02-26 22:49:51 -0800

    Update reviews.test.js

--- test/reviews.test.js
+++ test/reviews.test.js
@@ -1,11 +1,8 @@
 const axios = require('axios');
 const request = require("supertest");
 const server = require("../server/index.js");
-// require("dotenv").config();
-// const url = `http://localhost:${process.env.PORT}/reviews`;
 const product_id = 2;
 
-// jest.setTimeout(20000);
 
 test('test getReviews', async() => {
   const count = 500;
@@ -16,10 +13,6 @@ test('test getReviews', async() => {
     expect(response.statusCode).toBe(200);
     expect(reviewList.length).toBe(5);
     expect(reviewList[0].review_id).toBeDefined();
-    // const response = await axios.get(`${url}/${product_id}/${count}/${sort}`);
-    // expect(response.config.method).toBe('get');
-    // expect(response.data.results.length).toBe(5);
-    // expect(response.data.results[0].review_id).toBeDefined();
   }
   catch (err) {
     console.log(err)
@@ -34,10 +27,6 @@ test('test getReviewMeta', async() => {
     expect(response.statusCode).toBe(200);
     expect(reviewMeta.product_id).toBe(product_id);
     expect(reviewMeta.ratings).toBeDefined();
-    // const response = await axios.get(`${url}/meta/${product_id}`);
-    // expect(response.config.method).toBe('get');
-    // expect(response.data.product_id).toBe(product_id);
-    // expect(response.data.ratings).toBeDefined();
   }
   catch (err) {
     console.log(err)
@@ -64,8 +53,6 @@ test('test postReview', async() => {
   try {
     const response = await request(server).post(`/reviews`).send(review)
     expect(response.statusCode).toBe(201);
-    // const response = await axios.post(url, review);
-    // expect(response.config.method).toBe('post');
   }
   catch (err) {
     console.log(err)
@@ -78,8 +65,6 @@ test('test putReviewReport', async() => {
   try {
     const response = await request(server).put(`/reviews/${review_id}/report`).send({review_id})
     expect(response.statusCode).toBe(204);
-    // const response = await axios.put(`${url}/${review_id}/report`, {review_id});
-    // expect(response.config.method).toBe('put');
   }
   catch (err) {
     console.log(err)
@@ -92,8 +77,6 @@ test('test putReviewHelpfulness', async() => {
   try {
     const response = await request(server).put(`/reviews/${review_id}/helpful`).send({review_id})
     expect(response.statusCode).toBe(204);
-    // const response = await axios.put(`${url}/${review_id}/helpful`, {review_id});
-    // expect(response.config.method).toBe('put');
   }
   catch (err) {
     console.log(err)
